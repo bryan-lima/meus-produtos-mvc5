4dc34e344dce7df836823d7a33f4afe6a347eaa1
Bryan <bryanlima200@outlook.com>
2022-09-21 20:02:56 -0300

    enable-migrations

---
+++ src/DevIO.Infra/Data/Migrations/Configuration.cs
@@ -0,0 +1,21 @@
+﻿using DevIO.Infra.Data.Context;
+using System.Data.Entity.Migrations;
+
+namespace DevIO.Infra.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<MeuDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(MeuDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
