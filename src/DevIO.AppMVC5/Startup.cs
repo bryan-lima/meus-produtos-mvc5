3f4ecef5607487281b27897831c363371a636bd0
Bryan <bryanlima200@outlook.com>
2022-09-21 19:09:15 -0300

    Adicionado projeto DevIO.AppMVC5

---
+++ src/DevIO.AppMVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevIO.AppMVC5.Startup))]
+namespace DevIO.AppMVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
