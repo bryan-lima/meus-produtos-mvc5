96e9b94183527a26c335b3b11462d72b4c380add
Bryan <bryanlima200@outlook.com>
2022-09-24 14:53:39 -0300

    Resolvido problema de DI (usando SimpleInjector) ao tentar subir aplicação

--- src/DevIO.AppMVC5/Controllers/AccountController.cs
+++ src/DevIO.AppMVC5/Controllers/AccountController.cs
@@ -18,16 +18,6 @@ namespace DevIO.AppMVC5.Controllers
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
-        public AccountController()
-        {
-        }
-
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
-        {
-            UserManager = userManager;
-            SignInManager = signInManager;
-        }
-
         public ApplicationSignInManager SignInManager
         {
             get
